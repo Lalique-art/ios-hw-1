97645519dce2bf341532c469cf0dda38cee2f7c5
maryam matiri <64448234+Lalique-art@users.noreply.github.com>
2022-03-08 22:24:17 +0300

    done

--- Homework 1/Homework 1/ContentView.swift
+++ Homework 1/Homework 1/ContentView.swift
@@ -9,10 +9,104 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack{
+            Text("MY FAV MOVIES")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding()
+            Image(systemName:"heart")
+                .resizable()
+                .foregroundColor(Color.pink)
+                .frame(width: 100, height: 80)
+            Spacer()
+           
+            HStack{
+                Image("gone with the wind")
+                    .resizable()
+                    .frame(width:114.0 , height: 114.0)
+                Text("    Gone with the wind")
+                    .font(.title2)
+                    
+                Text("10")
+                    .foregroundColor(Color.pink)
+                    .multilineTextAlignment(.trailing)
+                Image(systemName: "star")
+                    .foregroundColor(Color.yellow)
+            }
+            VStack{
+                HStack{
+                    Image("pride")
+                        .resizable()
+                        .frame(width: 114, height: 114)
+                    Text("    pride and prejudice")
+                        .font(.title2)
+                    Text("10")
+                        .foregroundColor(Color.pink)
+                    Image(systemName:"star")
+                        .foregroundColor(Color.yellow)
+                
+                }
+                VStack{
+                    HStack{
+                        Image("vampire")
+                            .resizable()
+                            .frame(width: 114, height: 114)
+                        Text("   interview with a vampire")
+                            .font(.headline)
+                        Text("9")
+                            .foregroundColor(Color.pink)
+                        Image(systemName:"star")
+                            .foregroundColor(Color.yellow)
+                    }
+                }
+                VStack{
+                    HStack{
+                        Image("little women")
+                            .resizable()
+                            .frame(width: 114, height: 114)
+                        Text("              little women")
+                            .font(.title2)
+                        Text("100")
+                            .foregroundColor(Color.pink)
+                        Image(systemName:"star")
+                            .foregroundColor(Color.yellow)
+                    }
+                }
+                VStack{
+                    HStack{
+                        Image("harry potter")
+                            .resizable()
+                            .frame(width: 114, height: 114)
+                        Text("                          harry potter")
+                            .font(.headline)
+                        Text("10")
+                            .foregroundColor(Color.pink)
+                        Image(systemName:"star")
+                            .foregroundColor(Color.yellow)
+                    }
+                }
+                    
+                    
+                    
+                
+        }
     }
 }
+    
+}
+        
+                
+
+        
+                
+            
+
+
+
+
+    
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- Homework 1/Homework 1/Homework_1App.swift
+++ Homework 1/Homework 1/Homework_1App.swift
@@ -10,8 +10,9 @@ import SwiftUI
 @main
 struct Homework_1App: App {
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+       WindowGroup {
+           ContentView()
+        
         }
     }
 }
